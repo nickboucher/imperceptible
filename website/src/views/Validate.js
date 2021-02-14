0d3211b273d36cc88481adac4d683f84126d273a
Nicholas Boucher <nicholas.d.boucher@gmail.com>
2021-02-14 00:53:24 +0000

    Skeleton Validate Page

--- website/src/views/Validate.js
+++ website/src/views/Validate.js
@@ -1,4 +1,4 @@
-import React from "react";
+import {useState} from "react";
 
 // core components
 import IndexNavbar from "components/Navbars/IndexNavbar.js";
@@ -9,14 +9,19 @@ import {
   Container,
   Row,
   Col,
+  Form,
+  FormGroup,
+  Label,
+  Input
 } from "reactstrap";
 
 export default function Validate() {
+  const [input, setInput] = useState("");
   return (
     <>
       <IndexNavbar />
       <div className="wrapper">
-        <div className="page-header">
+        <div className="page-header d-flex flex-wrap">
           <img
             alt="..."
             className="dots"
@@ -33,6 +38,37 @@ export default function Validate() {
                     <h1><b>Validate</b></h1>
                   </Col>
               </Row>
+              <Form>
+                <FormGroup row className="mt-5 mb-n2">
+                    <Label for="inputText" md={2}><h2>Input&nbsp;={'>'}</h2></Label>
+                    <Col md={10}>
+                        <Input
+                            type="textarea"
+                            name="inputText"
+                            id="inputText"
+                            placeholder="Paste some text here to validate whether it has been manipulated with imperceptible perturbations..."
+                            onChange={e => setInput(e.target.value)}
+                        />
+                    </Col>
+                </FormGroup>
+            </Form>
+            <Row className="mt-4 pt-4 mb-4 pb-4" style={{ display: input.length ? null : 'None' }}>
+                <Col md={2}>
+                    <h3>Output&nbsp;={'>'}</h3>
+                </Col>
+                <Col md={10}>
+                    <div className="generated">
+                        Not Yet Implemented.
+                    </div>
+                </Col>
+            </Row>
+          </Container>
+          <Container className="pt-0 pb-4 align-self-end">
+            <Row>
+                <Col md={{ size: 10, offset: 2}}>
+                  <p className="text-muted">This tool tests whether the input string contains encodings that may be indicators of imperceptible perturbations. It is not guaranteed to detect all forms of imperceptible perturbations. All text entered remains on your local machine. Nothing is transmitted to or logged on any server. This tool is for academic purposes only and the user holds sole responsibility for how it is used.</p>
+                </Col>
+            </Row>
         </Container>
       </div>
       <Footer />
